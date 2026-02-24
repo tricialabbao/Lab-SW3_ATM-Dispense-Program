33d87f156476ede07142516173978b6abb8ffbb1
Tricia A. Labbao <tricialabbao.05@gmail.com>
2026-02-24 01:17:30 +0000

    hy

--- Chain of Responsibility/BPI_Atm.java
+++ Chain of Responsibility/BPI_Atm.java
@@ -2,7 +2,7 @@ public class BPI_Atm {
     public static void main(String[] args) {
         ATMDispenseChain atmDispenser = new ATMDispenseChain();
         
-        int amount = 2870;
+        int amount = 250;
                 if (amount % 10 != 0) {
             System.out.println("Amount should be a multiple of 10.");
             return;
